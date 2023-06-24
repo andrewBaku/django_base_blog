6c7d9d6ad44691c731d2abc7c1e2ca4902333158
Andrii Baku <bakuandrej25@gmail.com>
2023-06-24 22:46:22 +0300

    Add detail page for all individual post

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,8 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.order_by('created_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
