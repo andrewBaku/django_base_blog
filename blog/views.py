320a8180d68d3c445ce0d37e5d91ab35aa66e81f
Andrii Baku <bakuandrej25@gmail.com>
2023-06-27 16:38:44 +0300

    Create sign up functional.

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,27 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
 # Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/user_creation_form.html', {'form': form})
+
 
 def post_list(request):
     posts = Post.objects.order_by('created_date')
@@ -87,3 +104,5 @@ def approve_comment(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
+
+
--- djgirls_blog/urls.py
+++ djgirls_blog/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from blog.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/signup/', signup, name='signup'),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('account/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls'))
